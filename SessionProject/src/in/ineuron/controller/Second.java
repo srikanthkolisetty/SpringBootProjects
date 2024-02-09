1f960ec7187f3b51623506a625e2337ad3336343
Srikanth Kolisetty <82137255+srikanthkolisetty@users.noreply.github.com>
2024-02-09 11:24:41 +0530

    Add files via upload

---
+++ SessionProject/src/in/ineuron/controller/Second.java
@@ -0,0 +1,38 @@
+package in.ineuron.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Second
+ */
+@WebServlet("/second")
+public class Second extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		PrintWriter writer = response.getWriter();
+	
+		writer.println(session.getAttribute("name"));
+		writer.println(request.getAttribute("namee"));
+		session.setAttribute("lastname", request.getParameter("lastname"));
+		writer.println(request.getParameter("name"));
+		writer.println(request.getContextPath());
+	
+	}
+
+}
